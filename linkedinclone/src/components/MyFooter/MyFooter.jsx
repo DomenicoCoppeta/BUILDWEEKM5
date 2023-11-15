d35460605cdf1cf304777d2fe140a837ece26863
DomenicoCoppeta <domenicocoppeta@gmail.com>
2023-11-15 19:52:38 +0100

    bozza footer

--- linkedinclone/src/components/MyFooter/MyFooter.jsx
+++ linkedinclone/src/components/MyFooter/MyFooter.jsx
@@ -43,8 +43,7 @@ return (
                 </ul>
             </div>
             <div className="col-md-2 mb-md-0 mb-3">
-                <DropdownButton className='col-md-4' id="dropdown-item-button" title="Dropdown button">
-                    <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+                <DropdownButton className='col-md-4' id="dropdown-item-button" title="Italiano(Italiano)">
                         <Dropdown.Item as="button">Action</Dropdown.Item>
                         <Dropdown.Item as="button">Another action</Dropdown.Item>
                         <Dropdown.Item as="button">Something else</Dropdown.Item>
